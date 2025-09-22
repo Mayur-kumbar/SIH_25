7074d7b993fbfe10f37ab9a80c7cb44479ca80f8
Mayur-kumbar <mayurkumbhar738@gmail.com>
2025-09-22 21:13:25 +0530

    added user model

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    accountType: {
+      type: String,
+      enum: ["student", "teacher", "admin"], 
+      default: "student",
+    },
+    department: {
+      type: String,
+      enum: ["Computer Science", "Electronics", "Mechanical", "Civil"], 
+      required: function () {
+        return this.accountType === "student" || this.accountType === "teacher";
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);

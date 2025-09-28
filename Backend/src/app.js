031ef94d6d3c81becfc6f70582f653c5b629f9c2
Mayur-kumbar <mayurkumbhar738@gmail.com>
2025-09-28 18:34:24 +0530

    latest changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,20 +11,19 @@ app.use(
     })
 );
 
-app.use(express.static("public"));
-
-app.use(cookieParser());
-
-import authRoutes from "./routes/auth.route.js"
-import activityRoutes from "./routes/activity.route.js"
-import userRoutes from "./routes/user.route.js"
-
-app.use("/api/auth", authRoutes);
-app.use("/api/activity", activityRoutes)
-app.use("/api/user", userRoutes);
-
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
+import authRoutes from "./routes/auth.route.js";
+import activityRoutes from "./routes/activity.route.js";
+import userRoutes from "./routes/user.route.js";
+
+app.use("/api/auth", authRoutes);
+app.use("/api/activity", activityRoutes);
+app.use("/api/user", userRoutes);
 
 export { app };
